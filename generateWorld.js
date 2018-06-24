8accb703cf3c8cf4bfb92a1e287523009a05df70
James Edwards <james.howe.edwards@gmail.com>
2018-06-24 07:09:41 -0700

    Add npm scripts for world generator

--- generateWorld.js
+++ generateWorld.js
@@ -1,6 +1,21 @@
 const generator = require('./generator/generate.js');
+const { execSync } = require('child_process');
 
 const fileName = process.argv[2];
 const outputName = process.argv[3];
 
-generator(fileName, outputName);
+if (!fileName && !outputName) {
+  console.log(`
+    -- WORLD GENERATOR --
+    npm run generate-world {input-file} {output-file}
+    
+    to see example world script:
+    npm run generate-world -- --example
+  `)
+} else if (fileName === '--example') {
+  const result = execSync('cat example.txt');
+  console.log(result.toString());
+} else {
+  generator(fileName, outputName);
+}
+
